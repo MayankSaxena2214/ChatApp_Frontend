3893210084fc1e070268ea4521384ef651b34319
Mayank Saxena <mayanksaxena2214@gmail.com>
2024-12-31 18:18:46 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-export const server='http://localhost:3030';
+// export const server='http://localhost:3030';
+export const server='https://chatapp-backend-rgka.onrender.com';
 import {Provider} from "react-redux"
 import store from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react'
